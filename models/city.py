8d3cfabc2e7f1ab507a10cc14171175510d71163
David Muindi <muindidiego@gmail.com>
2023-06-20 05:00:05 -0700

    Task 8

--- models/city.py
+++ models/city.py
@@ -1,8 +1,17 @@
-from sqlalchemy import Column, String, ForeignKey
+#!/usr/bin/python3
+"""
+Module for City class
+"""
+
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 class City(BaseModel, Base):
+    """City class"""
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
+
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities")
 
