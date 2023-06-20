3a0f492e14abd9f21e9258409855f61fd4c40923
David Muindi <muindidiego@gmail.com>
2023-06-20 05:37:09 -0700

    Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Module: amenity
+Defines the Amenity class, representing an amenity in the application.
+"""
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Amenity class representing an amenity in the application.
+
+    Attributes:
+        __tablename__ (str): Name of the table in the database.
+        name (str): Name of the amenity.
+        place_amenities (relationship): Relationship with the Place class through the place_amenity table.
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary='place_amenity', back_populates='amenities')
+
--- models/place.py
+++ models/place.py
@@ -5,8 +5,12 @@ Defines the Place class, representing a place in the application.
 """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """
@@ -24,7 +28,7 @@ class Place(BaseModel, Base):
         price_by_night (int): Price per night for the place.
         latitude (float): Latitude coordinate of the place.
         longitude (float): Longitude coordinate of the place.
-        reviews (relationship): Relationship with the Review class.
+        amenities (relationship): Relationship with the Amenity class.
     """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -37,5 +41,5 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship('Review', cascade='all, delete', backref='place')
+    amenities = relationship('Amenity', secondary='place_amenity', viewonly=False, back_populates='place')
 
