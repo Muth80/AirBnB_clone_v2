93f9b94e81783bbf680997946ef3e505898e6c5f
David Muindi <muindidiego@gmail.com>
2023-06-20 03:20:32 -0700

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,12 @@
+-- Create the hbnb_dev_db database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- Create the hbnb_dev user with the password hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Grant all privileges on the hbnb_dev_db database to hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
+-- Grant SELECT privilege on the performance_schema database to hbnb_dev user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
-REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_dev'@'localhost';
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
-FLUSH PRIVILEGES;
