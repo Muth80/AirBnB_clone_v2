f0e60d7c6a268a575d5e085c34d5c42c98cc33bd
David Muindi <muindidiego@gmail.com>
2023-07-07 02:57:31 -0700

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
+"""
+This module contains a Fabric script that generates a .tgz archive
+from the contents of the web_static folder.
+"""
+
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        The path of the archive if it was successfully generated, None otherwise.
     """
-
-    # create the versions directory if it doesn't exist
     local("mkdir -p versions")
-
-    # format the current datetime to use in the filename of our archive
     now = datetime.now()
-    date_string = now.strftime("%Y%m%d%H%M%S")
-    output_file = "versions/web_static_{}.tgz".format(date_string)
-
-    print("Packing web_static to {}".format(output_file))
-
-    # use the tar command to create a .tgz archive
-    local("tar -cvzf {} web_static".format(output_file))
-
-    print("{} packed: {} -> {}Bytes".format("web_static",
-                                            output_file,
-                                            local("ls -lh {}".format(output_file))))
-
-    return output_file
-```
+    file_name = "web_static_{}.tgz".format(
+        now.strftime("%Y%m%d%H%M%S")
+    )
+    try:
+        local("tar -czvf versions/{} web_static".format(file_name))
+        return "versions/{}".format(file_name)
+    except Exception:
+        return None
+`
