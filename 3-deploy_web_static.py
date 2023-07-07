ba15aedeb3c0cc91c7accdd66ef42091cec6508b
David Muindi <muindidiego@gmail.com>
2023-07-07 04:05:54 -0700

    Update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 """
-This module contains a Fabric script that distributes an archive to
-web servers using the do_deploy function.
+This module contains a Fabric script that creates and distributes an
+archive to web servers using the deploy function.
 """
 
 from fabric.api import *
-from os.path import exists
+from fabric.contrib.files import exists
+from datetime import datetime
 
 
 env.hosts = ['100.26.157.96', '54.160.71.55']
 env.user = 'ubuntu'
 env.key_filename = 'https://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@github.com/Muth80/alx-system_engineering-devops.git'
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        The path of the archive if it was successfully generated, None otherwise.
+    """
+    local("mkdir -p versions")
+    now = datetime.now()
+    file_name = "web_static_{}.tgz".format(
+        now.strftime("%Y%m%d%H%M%S")
+    )
+    try:
+        local("tar -czvf versions/{} web_static".format(file_name))
+        return "versions/{}".format(file_name)
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -53,8 +73,20 @@ def do_deploy(archive_path):
         # Create a new symbolic link
         run('ln -s {} /data/web_static/current'.format(path_no_ext))
 
-        print('New version deployed!')
         return True
     except Exception:
         return False
+
+
+def deploy():
+    """
+    Creates and distributes an archive to web servers.
+
+    Returns:
+        True if all operations have been done correctly, otherwise False.
+    """
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
 
