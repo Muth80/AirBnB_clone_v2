94bfcb81a2b2f4c36eac8665d4242f4fbad610a3
David Muindi <muindidiego@gmail.com>
2023-07-24 02:43:45 -0700

    Task 4

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -9,8 +9,8 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# Import the routes defined in the 0-hello_route.py file
-from web_flask.0-hello_route import *
+# Import the routes defined in the 4-number_route.py file
+from web_flask.4_number_route import *
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
